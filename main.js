aa4f7428af41a3bab5be3708bfb576879b430eb5
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2021-09-18 08:54:20 +1000

    Updated to latest version of electron

--- main.js
+++ main.js
@@ -2,44 +2,42 @@
 
 const config = require('./config.json');
 
-const electron = require('electron');
-const {app, protocol} = require('electron');
-const BrowserWindow = electron.BrowserWindow;
+const { app, protocol, BrowserWindow } = require('electron');
 
 const path = require('path')
 const url = require('url')
 
 var mainWindow = null;
 
-function createWindow () {
+function createWindow() {
 
-  mainWindow = new BrowserWindow({width: 800, height: 640, resizable: false, autoHideMenuBar: true})
-  mainWindow.setMenu(null);
-  
-  if (config.mode == "debug") {
-     mainWindow.webContents.openDevTools();
- }
- 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    mainWindow = new BrowserWindow({ width: 800, height: 640, resizable: false, autoHideMenuBar: true })
+    mainWindow.setMenu(null);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    if (config.mode == "debug") {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 
 }
- 
+
 app.on('ready', createWindow)
 
-app.on('window-all-closed',  () => {
-  app.quit()
+app.on('window-all-closed', () => {
+    app.quit()
 })
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
